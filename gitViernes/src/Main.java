6fbfd8eeeccf597d88b793c2341d978511f192c0
samidhdk <>
2024-10-18 10:10:23 +0200

    samidhdk hello world

--- gitViernes/src/Main.java
+++ gitViernes/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("alba dice hola");
+        System.out.println("samidhdk dice hola");
     }
 }
